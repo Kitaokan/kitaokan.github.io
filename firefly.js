691cd6cdfed452e0b0d2cf3ef3bc51a868350daf
Naoki Kitaoka <88298025+Kitaokan@users.noreply.github.com>
2021-12-14 19:21:38 +0900

    add firefly

---
+++ firefly.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line func-style
+function setup() {}
+
+// eslint-disable-next-line func-style
+function draw() {
+  // eslint-disable-next-line no-undef
+  ellipse(50, 50, 80, 80);
+}
